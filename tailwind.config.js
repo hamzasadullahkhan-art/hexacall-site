7fc7d87df64d79f85049c93fbba9d2672b7bdb3b
hamzasadullahkhan-art <hamzasadullahkhan@gmail.com>
2025-12-16 19:15:03 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx}"
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
   ],
   theme: {
     extend: {}
   },
   plugins: []
-}
+};
